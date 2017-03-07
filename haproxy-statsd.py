cd70fc84c42e22208a6b7beb9d901cbc7f523b93
Brendan <brendan@getharvest.com>
2017-03-06 16:31:43 -0800

    Add session limit metrics.

--- haproxy-statsd.py
+++ haproxy-statsd.py
@@ -55,7 +55,7 @@ def report_to_statsd(stat_rows,
         path = '.'.join([namespace, row['pxname'], row['svname']])
 
         # Report each stat that we want in each row
-        for stat in ['scur', 'smax', 'ereq', 'econ', 'rate', 'req_rate', 'eresp', 'qcur', 'wredis', 'wretr', 'bin', 'bout', 'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'qtime', 'ctime', 'rtime', 'ttime']:
+        for stat in ['scur', 'slim', 'smax', 'ereq', 'econ', 'rate', 'req_rate', 'eresp', 'qcur', 'wredis', 'wretr', 'bin', 'bout', 'hrsp_1xx', 'hrsp_2xx', 'hrsp_3xx', 'hrsp_4xx', 'hrsp_5xx', 'qtime', 'ctime', 'rtime', 'ttime']:
             val = row.get(stat) or 0
             udp_sock.sendto(
                 '%s.%s:%s|g' % (path, stat, val), (host, port))
